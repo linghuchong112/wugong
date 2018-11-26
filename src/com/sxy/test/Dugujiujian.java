5b66e6d751cbc244ad996a7668c87e7045422d34
linghuchong <1172539427@qq.com>
2018-11-26 22:37:32 +0800

    更新2

--- src/com/sxy/test/Dugujiujian.java
+++ src/com/sxy/test/Dugujiujian.java
@@ -5,6 +5,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("111111111");
+		System.out.println("222222222");
+		
 	}
 
 }
